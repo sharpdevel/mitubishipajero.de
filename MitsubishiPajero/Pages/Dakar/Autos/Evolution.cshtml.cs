e2831cf886d4b694238aff584ad1255474fbc5b4
Tobias Mundt <tobias.mundt@outlook.com>
2020-03-26 02:01:25 +0100

    added all dakar posts to project

---
+++ MitsubishiPajero/Pages/Dakar/Autos/Evolution.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MitsubishiPajero.Pages.Shared;
+
+namespace MitsubishiPajero.Pages.Dakar.Autos
+{
+    public class EvolutionModel : PageModel
+    {
+        //Properties
+        #region GalleryVM
+        public GalleryPartialModel GalleryVM
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        //Methods
+        #region OnGet
+        public void OnGet()
+        {
+            this.GalleryVM = new GalleryPartialModel("/images/dakar/cars/evolution/");
+        }
+        #endregion
+    }
+}
---
+++ MitsubishiPajero/Pages/Dakar/Rueckblick/Dakar1986.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MitsubishiPajero.Pages.Dakar.Rueckblick
+{
+    public class Dakar1986Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
